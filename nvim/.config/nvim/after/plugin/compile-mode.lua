b89632e51e85b8b96907e2f20de7b838093e771e
Ivan Filip Terzic <i.terzic@gmx.net>
2024-02-27 16:51:37 +0100

    fix(nvim): old config sucks

--- nvim/.config/nvim/after/plugin/compile-mode.lua
+++ nvim/.config/nvim/after/plugin/compile-mode.lua
@@ -3,14 +3,7 @@ compile_mode.setup({
     -- you can disable colors by uncommenting this line
     no_baleia_support = true,
     default_command = "make -k",
-    same_window_errors = true,
 })
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "compilation",
-  command = "wincmd w",
-})
-
 
 vim.keymap.set('n', '<leader>ne', function() compile_mode.next_error() end, {})
 vim.keymap.set('n', '<leader>pe', function() compile_mode.prev_error() end, {})
