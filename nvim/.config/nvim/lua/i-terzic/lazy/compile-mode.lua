e1b8f4d736f9f31aa6b505fb7bdc611d0cd7b407
Ivan Filip Terzic <i.terzic@gmx.net>
2024-07-14 19:16:03 +0200

    feat(nvim): add colors to compile-mode

--- nvim/.config/nvim/lua/i-terzic/lazy/compile-mode.lua
+++ nvim/.config/nvim/lua/i-terzic/lazy/compile-mode.lua
@@ -4,11 +4,10 @@ return {
     local compile_mode = require("compile-mode")
     compile_mode.setup({
       -- you can disable colors by uncommenting this line
-      no_baleia_support = true,
+      -- no_baleia_support = true,
       default_command = "make -k",
       same_window_errors = true,
     })
-
     vim.keymap.set("n", "<leader>ne", function()
       compile_mode.next_error()
     end, {})
@@ -17,4 +16,7 @@ return {
     end, {})
   end,
   lazy = false,
+  dependencies = {
+    { "m00qek/baleia.nvim", tag = "v1.3.0" },
+  }
 }
