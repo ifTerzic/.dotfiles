b89d4ed4b83223f1bb78b40056397006ba004413
Ivan Filip Terzic <i.terzic@gmx.net>
2024-08-17 21:38:38 +0200

    fix: conform.nvim & compile-mode.nvim

--- nvim/.config/nvim/lua/i-terzic/lazy/compile-mode.lua
+++ nvim/.config/nvim/lua/i-terzic/lazy/compile-mode.lua
@@ -1,13 +1,42 @@
+-- return {
+--   "ej-shafran/compile-mode.nvim",
+--   config = function()
+--     vim.g.compile_mode = {
+--       -- you can disable colors by uncommenting this line
+--       -- no_baleia_support = true,
+--       default_command = "make -k",
+--     }
+--     vim.keymap.set("n", "<leader>ne", function()
+--       compile_mode.next_error()
+--     end, {})
+--     vim.keymap.set("n", "<leader>pe", function()
+--       compile_mode.prev_error()
+--     end, {})
+--   end,
+--   lazy = false,
+--   dependencies = {
+--     { "m00qek/baleia.nvim", tag = "v1.3.0" },
+--   }
+-- }
+--
 return {
   "ej-shafran/compile-mode.nvim",
+  branch = "latest",
+  -- or a specific version:
+  -- tag = "v4.0.0"
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    -- if you want to enable coloring of ANSI escape codes in
+    -- compilation output, add:
+    { "m00qek/baleia.nvim", tag = "v1.3.0" },
+  },
   config = function()
-    local compile_mode = require("compile-mode")
-    compile_mode.setup({
-      -- you can disable colors by uncommenting this line
-      -- no_baleia_support = true,
-      default_command = "make -k",
-      same_window_errors = true,
-    })
+    ---@type CompileModeOpts
+    vim.g.compile_mode = {
+        -- to add ANSI escape code support, add:
+        baleia_setup = true,
+    }
+
     vim.keymap.set("n", "<leader>ne", function()
       compile_mode.next_error()
     end, {})
@@ -15,8 +44,6 @@ return {
       compile_mode.prev_error()
     end, {})
   end,
-  lazy = false,
-  dependencies = {
-    { "m00qek/baleia.nvim", tag = "v1.3.0" },
-  }
+  lazy = false
 }
+
--- nvim/.config/nvim/lua/i-terzic/lazy/lsp.lua
+++ nvim/.config/nvim/lua/i-terzic/lazy/lsp.lua
@@ -1,91 +1,91 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "j-hui/fidget.nvim",
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v4.x',
+    lazy = true,
+    config = false,
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
   },
 
-  config = function()
-    local cmp = require("cmp")
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'},
+    },
+    config = function()
+      local cmp = require('cmp')
 
-    require("fidget").setup({})
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-      },
-      handlers = {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
+      cmp.setup({
+        sources = {
+          {name = 'nvim_lsp'},
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+      })
+    end
+  },
 
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                runtime = { version = "Lua 5.1" },
-                diagnostics = {
-                  globals = { "vim", "it", "describe", "before_each", "after_each" },
-                },
-              },
-            },
-          })
-        end,
-      },
-    })
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
 
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      -- lsp_attach is where you enable features that only work
+      -- if there is a language server active in the file
+      local lsp_attach = function(client, bufnr)
+        local opts = {buffer = bufnr}
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- For luasnip users.
-      }, {
-        { name = "buffer" },
-      }),
-    })
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      end
 
-    vim.diagnostic.config({
-      -- update_in_insert = true,
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
-    })
-  end,
+      lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
+      })
+
+      require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+          -- this first function is the "default handler"
+          -- it applies to every language server without a "custom handler"
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+        }
+      })
+    end
+  }
 }
+
