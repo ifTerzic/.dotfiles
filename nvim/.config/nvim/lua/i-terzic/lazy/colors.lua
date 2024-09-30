36bc8be87e84e669fa71e35f2c97f45927e388e9
Ivan Filip Terzic <i.terzic@gmx.net>
2024-09-30 18:46:54 +0200

    refactor(nvim): remove unused plugins

--- nvim/.config/nvim/lua/i-terzic/lazy/colors.lua
+++ nvim/.config/nvim/lua/i-terzic/lazy/colors.lua
@@ -1,13 +1,13 @@
 function ColorMyPencils(color)
-  color = color or "onedark"
-  vim.cmd.colorscheme(color)
+	color = color or "nightfox"
+	vim.cmd.colorscheme(color)
 end
 
 return {
-  {
-    "navarasu/onedark.nvim",
-    config = function()
-      ColorMyPencils()
-    end,
-  },
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			ColorMyPencils()
+		end,
+	},
 }
