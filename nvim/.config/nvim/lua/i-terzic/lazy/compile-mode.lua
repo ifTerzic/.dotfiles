f4de2c5fba190a6428d7fe0cfb2d42c1445af91b
Ivan Filip Terzic <i.terzic@gmx.net>
2024-12-26 23:41:50 +0100

    feat(nvim): add remaps to compile-mode.nvim

--- nvim/.config/nvim/lua/i-terzic/lazy/compile-mode.lua
+++ nvim/.config/nvim/lua/i-terzic/lazy/compile-mode.lua
@@ -1,49 +1,39 @@
--- return {
---   "ej-shafran/compile-mode.nvim",
---   config = function()
---     vim.g.compile_mode = {
---       -- you can disable colors by uncommenting this line
---       -- no_baleia_support = true,
---       default_command = "make -k",
---     }
---     vim.keymap.set("n", "<leader>ne", function()
---       compile_mode.next_error()
---     end, {})
---     vim.keymap.set("n", "<leader>pe", function()
---       compile_mode.prev_error()
---     end, {})
---   end,
---   lazy = false,
---   dependencies = {
---     { "m00qek/baleia.nvim", tag = "v1.3.0" },
---   }
--- }
---
 return {
-  "ej-shafran/compile-mode.nvim",
-  branch = "latest",
-  -- or a specific version:
-  -- tag = "v4.0.0"
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    -- if you want to enable coloring of ANSI escape codes in
-    -- compilation output, add:
-    { "m00qek/baleia.nvim", tag = "v1.3.0" },
-  },
-  config = function()
-    ---@type CompileModeOpts
-    vim.g.compile_mode = {
-        -- to add ANSI escape code support, add:
-        baleia_setup = true,
-    }
+	"ej-shafran/compile-mode.nvim",
+	branch = "latest",
+	-- or a specific version:
+	-- tag = "v4.0.0"
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- if you want to enable coloring of ANSI escape codes in
+		-- compilation output, add:
+		{ "m00qek/baleia.nvim", tag = "v1.3.0" },
+	},
+	config = function(args)
+		---@type CompileModeOpts
+		vim.g.compile_mode = {
+			-- to add ANSI escape code support, add:
+			baleia_setup = true,
+			error_regexp_table = {
+				typescript = {
+					-- TypeScript errors take the form
+					-- "path/to/error-file.ts(13,23): error TS22: etc."
+					regex = "^\\(.\\+\\)(\\([1-9][0-9]*\\),\\([1-9][0-9]*\\)): error TS[1-9][0-9]*:",
+					filename = 1,
+					row = 2,
+					col = 3,
+				},
+			},
+		}
 
-    vim.keymap.set("n", "<leader>ne", function()
-      compile_mode.next_error()
-    end, {})
-    vim.keymap.set("n", "<leader>pe", function()
-      compile_mode.prev_error()
-    end, {})
-  end,
-  lazy = false
+		vim.keymap.set("n", "<leader>cn", function()
+			require("compile-mode").next_error()
+		end, {})
+		vim.keymap.set("n", "<leader>cp", function()
+			require("compile-mode").prev_error()
+		end, {})
+		vim.keymap.set("n", "<M-c>", "<CMD>Compile<CR>")
+		vim.keymap.set("n", "<M-r>", "<CMD>Recompile<CR>")
+	end,
+	lazy = false,
 }
-
